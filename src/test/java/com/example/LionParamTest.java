85cdba062814d1e2a8c88f12f29054df5f53c9a0
Natalya Pryakhina <nat4958@mail.ru>
2026-04-23 08:24:01 +0500

    Добавлен отчет JaCoCo и тесты для Cat, Lion, Feline

---
+++ src/test/java/com/example/LionParamTest.java
@@ -0,0 +1,34 @@
+import com.example.Feline;
+import com.example.Lion;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.mockito.Mockito;
+import static org.junit.Assert.assertEquals;
+
+@RunWith(Parameterized.class)
+public class LionParamTest {
+    private final String sex;
+    private final boolean expectedHasMane;
+
+    public LionParamTest(String sex, boolean expectedHasMane) {
+        this.sex = sex;
+        this.expectedHasMane = expectedHasMane;
+    }
+
+    @Parameterized.Parameters
+    public static Object[][] getData() {
+        return new Object[][]{
+                {"Самец", true},
+                {"Самка", false}
+        };
+    }
+
+    @Test
+    public void shouldCheckHasMane() throws Exception {
+        Feline feline = Mockito.mock(Feline.class);
+        Lion lion = new Lion(sex, feline);
+        assertEquals(expectedHasMane, lion.doesHaveMane());
+    }
+}
+
